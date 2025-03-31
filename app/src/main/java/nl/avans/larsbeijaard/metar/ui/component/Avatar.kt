538bf560cef866e053be100fc9a805ad0ca0c129
Lars Devans <l.beijaard1@student.avans.nl>
2025-03-31 12:21:05 +0200

    Add loading state

--- app/src/main/java/nl/avans/larsbeijaard/metar/ui/component/Avatar.kt
+++ app/src/main/java/nl/avans/larsbeijaard/metar/ui/component/Avatar.kt
@@ -1,16 +1,18 @@
 package nl.avans.larsbeijaard.metar.ui.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
+import coil.compose.SubcomposeAsyncImage
 import nl.avans.larsbeijaard.metar.R
 import nl.avans.larsbeijaard.metar.ui.viewmodel.avatar.AvatarViewModel
 
@@ -20,17 +22,20 @@ fun Avatar(
     viewModel: AvatarViewModel = viewModel()
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    val painter = rememberAsyncImagePainter(
-        ImageRequest.Builder(LocalContext.current)
-            .data(uiState.avatarUrl)
-            .error(R.drawable.error_image)
-            .build()
-    )
 
-    Image(
-        painter = painter,
-        contentDescription = stringResource(R.string.avatar_image),
-        modifier = modifier,
-        contentScale = ContentScale.FillWidth
-    )
+    Box(modifier = modifier) {
+        SubcomposeAsyncImage(
+            model = uiState.avatarUrl,
+            contentDescription = stringResource(R.string.avatar_image),
+            loading = { CircularProgressIndicator() },
+            error = {
+                Image(
+                    painterResource(R.drawable.error_image),
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+    }
 }
