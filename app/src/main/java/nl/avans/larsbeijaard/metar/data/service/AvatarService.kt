5fcce4361f11e895bb0ee555523f6fbb80c747e1
Lars Devans <l.beijaard1@student.avans.nl>
2025-04-02 12:12:08 +0200

    Fix typo in string interpolation
    Co-authored-by: Linde Rus <130562052+linderus@users.noreply.github.com>

--- app/src/main/java/nl/avans/larsbeijaard/metar/data/service/AvatarService.kt
+++ app/src/main/java/nl/avans/larsbeijaard/metar/data/service/AvatarService.kt
@@ -1,5 +1,5 @@
 package nl.avans.larsbeijaard.metar.data.service
 
 fun getAvatarUrl(username: String, gender: String): String {
-    return "https://avatar.iran.liara.run/public/${gender}?username=$username}"
+    return "https://avatar.iran.liara.run/public/${gender}?username=${username}"
 }
